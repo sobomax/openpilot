11c21687594d294ea02ddc5d99efe49e364bbead
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-20 08:08:51 -0800

    Update to match 63d69d41 of selfdrive/controls/lib/pid.py in xps fork.

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -95,9 +95,9 @@ class PIController():
 
 class PIControllerNL(PIController):
   GainSaS_BP = [0., 3.9, 4., 5., 10., 20., 40.]
-  Gain_g = [0.15, .065, .085, .1, .12, .14, .16]
+  Gain_g = [0.05, .065, .085, .1, .12, .14, .16]
   GainV_BP = [0., 20., 20.01, 30.]
-  Gain_V = [0.3, .5, .65, 1.2]
+  Gain_V = [0.6, .85, 1.05, 1.2]
 
   @property
   def k_f(self):
